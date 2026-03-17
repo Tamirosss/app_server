4a8de90e25b3cdf869253f948ffddaf7527c9fe1
Tamirosss <162023296+Tamirosss@users.noreply.github.com>
2026-03-17 14:22:06 +0200

    Removed sqlite

--- obj/Debug/net8.0/JsonDemo.AssemblyInfo.cs
+++ obj/Debug/net8.0/JsonDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JsonDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3267146a9a9ef7b3f5474f0133ba5c80bfa7a9b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85f5a36b62da92bb065bbbf284ae38246b7d283c")]
 [assembly: System.Reflection.AssemblyProductAttribute("JsonDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JsonDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
